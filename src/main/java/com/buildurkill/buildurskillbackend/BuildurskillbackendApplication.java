b1741c4170472505b9d6deba13760280610725ee
“zacariya” <abuthahirtest123@gmail.com>
2025-01-31 20:19:11 +0530

    Initial commit

---
+++ src/main/java/com/buildurkill/buildurskillbackend/BuildurskillbackendApplication.java
@@ -0,0 +1,13 @@
+package com.buildurkill.buildurskillbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildurskillbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildurskillbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildurkill/buildurskillbackend/BuildurskillbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildurkill.buildurskillbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildurskillbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
